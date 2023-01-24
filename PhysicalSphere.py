1993662715464d2d4522a237df78f773ef4227e7
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-01-24 12:00:31 -0500

    Fix constructors for PhysicalMixin-derived objects

--- PhysicalSphere.py
+++ PhysicalSphere.py
@@ -4,23 +4,26 @@ import math
 from PhysicalMixin import PhysicalMixin, PhysicalPrimitiveType
 
 
-class PhysicalSphere(sphere, PhysicalMixin):
-    def __init__(self, **args):
-        if "mass" in args:
-            self.mass = args["mass"]
+class PhysicalSphere(PhysicalMixin, sphere):
+    def __init__(self, **kwargs):
+        # print('PhysicalSphere::__init__')
+        if "mass" in kwargs:
+            self.mass = kwargs["mass"]
 
         self.physical_primitive_type = PhysicalPrimitiveType.SPHERE
 
         # https://en.wikipedia.org/wiki/Drag_coefficient
         self._coeff_drag = 0.47
 
+        self.crosssectional_area = math.pi * self.radius**2
+
         # From vpython code, to support vpython shenanigans
-        args["_default_size"] = vector(1, 1, 1)
-        args["_objName"] = "sphere"
-        super(PhysicalSphere, self).setup(args)
+        kwargs["_default_size"] = vector(1, 1, 1)
+        kwargs["_objName"] = "sphere"
+
+        super(PhysicalSphere, self).__init__(**kwargs)
+        super(PhysicalSphere, self).setup(kwargs)
 
     @property
     def volume(self):
         return 4 / 3.0 * math.pi * self.radius**3
-
-
