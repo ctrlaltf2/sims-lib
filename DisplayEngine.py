069e8411ad78463fe6323a6b5053ad23b3f206cf
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-01-24 11:58:55 -0500

    Simplify displayengine code

--- DisplayEngine.py
+++ DisplayEngine.py
@@ -1,20 +1,23 @@
+from vpython import canvas
+
+from PhysicalMixin import np2vpy
+
 # Class that is solely responsible for rendering the world to VPython
 # Unfortunately has some side effects because of the way VPython works (it's a global singleton)
 class DisplayEngine:
-    _display_rate = 60  # Hz
+    # Vpython scene
+    scene = None
 
     _objects = []
 
     # constructor
-    def __init__(self, **kwargs):
-        if "display_rate" in kwargs:
-            assert kwargs["display_rate"] > 0  # display rates are positive.
-            self._display_rate = kwargs["display_rate"]
+    def __init__(self):
+        self.scene = canvas()
 
     # Run one iteration of the display loop
     def iterate(self):
         for obj in self._objects:
-            obj.pos = obj.position  # sample numpy position and set vpy position
+            obj.pos = np2vpy(obj.position)  # sample numpy position and set vpy position
 
-    def add_object(self, obj):
+    def register_object(self, obj):
         self._objects.append(obj)
